e00b2e73a27173f587c998ca7745601fcef64039
JIn Sun <callmesunjplz@gmail.com>
2014-04-06 23:33:26 -0500

    frame for findSource

--- findSource/views.py
+++ findSource/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+#def class IndexView(templateView):
+
+def index(request):
+    return HttpResponse('Hello, this is SourceFinder')
+
