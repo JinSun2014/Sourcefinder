6bde183b96738f5fdb8b49081daf3948c2c40ff0
JIn Sun <callmesunjplz@gmail.com>
2014-04-08 23:43:57 -0500

    Add reuslt page

--- findSource/urls.py
+++ findSource/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from findSource import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.IndexView.as_view(), name='index'),
+        url(r'^(?P<url>\S+)$', views.ResultView.as_view(), name='result'),
 )
--- findSource/views.py
+++ findSource/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
-#def class IndexView(templateView):
+class IndexView(TemplateView):
+    template_name = 'findSource/index.html'
 
-def index(request):
-    return HttpResponse('Hello, this is SourceFinder')
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
 
+class ResultView(ListView):
+    template_name = 'findSource/result.html'
+
+    def get_queryset(self):
+        return [1, 2, 3, 4, 5]
+
+    def get_context_data(self, **kwargs):
+        context = super(ResultView, self).get_context_data(**kwargs)
+        #context['url'] = self.kwargs(url)
+        return context
