358fa199fbc4e9d01a55a7209ab3def10e77ecd9
Jin Sun <callmesunjplz@gmail.com>
2014-04-13 20:04:12 -0500

    Add an enter event on homepage

--- findSource/static/findSource/js/findSource.js
+++ findSource/static/findSource/js/findSource.js
@@ -5,3 +5,13 @@ $("#search").click(function (){
         window.location.href = current_url + input;
     }
 });
+
+$('#user-input').keypress(function(e) {
+    if(e.which == 13) {
+        var input = $('#user-input').val();
+        if (input){
+            current_url = window.location.pathname;
+            window.location.href = current_url + input;
+        }
+    }
+});
