8c1ea54a068b0ccdcd9ebb161910aedf805a8f87
Jin Sun <callmesunjplz@gmail.com>
2014-04-14 13:35:36 -0500

    change some comfig for heroku

--- Sourcefinder/settings/__init__.py
+++ Sourcefinder/settings/__init__.py
@@ -81,7 +81,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        'static/',
+        'findSource/static/',
+)
 
 
 TEMPLATE_LOADERS = (
