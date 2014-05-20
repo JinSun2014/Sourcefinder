0ed2b9ad7de6dcabbc0548e1c33a73968a39d692
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-05-20 15:32:36 -0500

    replace aglorithm

--- findSource/AlchemyTest/alchemytest.py
+++ findSource/AlchemyTest/alchemytest.py
@@ -14,7 +14,8 @@ def readArticle(myUrl):
     response = alchemyapi.title("url", myUrl)
     output['title'] = response['title'].encode('utf-8', 'ignore')
     if output['title'] =='':
-    	output['title'] = 'No title found'
+    	output['title']='No title found'
+
 
     response = alchemyapi.author("url", myUrl)
 
@@ -34,79 +35,104 @@ def readArticle(myUrl):
 #Get People, takes the url and returns all the people (and potentially data) identified in the response entities
 def GetPeople(theUrl):
 
-	#added by Xiaofeng Zhu
-	responseText = alchemyapi.text('url',theUrl)
-
-	if responseText['status'] == 'OK':
-		cleanText=responseText['text'].encode('utf-8', 'ignore')
-	#added by Xiaofeng Zhu
-
-	response = alchemyapi.entities("url", theUrl, { 'quotations':1 })
 	person_list = []
 
-	if response['status'] == 'OK':
-		for entity in response['entities']:
+	people_response = alchemyapi.entities("url", theUrl, { 'quotations':1 })
+
+	name_quotation={}
+	if people_response['status'] == 'OK':
+
+		for entity in people_response['entities']:
 			if entity['type'] == 'Person':
 
-				#added by Xiaofeng Zhu
-				personDic={}
-				
-				personDic['name'] = entity['text'].encode('utf-8', 'ignore')
-
-				person=entity['text'].encode('utf-8').replace(" ", "%20")
-				personDic['twitterLink']="https://twitter.com/search?q="+person+"&src=corr&mode=users"
-				personDic['linkedInLink']="https://www.linkedin.com/vsearch/p?type=people&keywords="+person				
-				personDic['FacebookLink']="https://www.facebook.com/search/more/?q="+person
-
-				# personDic['subType']=""
-				# if entity.get('disambiguated'):
-				# 	if entity['disambiguated'].get('subType'):
-				# 		personDic['subType']=entity['disambiguated']['subType'][0]
-
-				personDic['quotation']="N/A"
 				if entity.get('quotations'):
-					personDic['quotation']=entity['quotations'][0]['quotation']
-				personDic['job_title']="N/A"
-				personDic['company']="N/A"
-
-				#Match job title
-				location=cleanText.find(entity['text'].encode('utf-8', 'ignore'))
-				subText=cleanText[location-40:location+40]
-				subresponse = alchemyapi.entities('text', subText, {'quotations':1 });
-
-				if subresponse['status'] == 'OK':
-
-					for job_com_entity in subresponse['entities']:
-						if (job_com_entity['type'] =='JobTitle'):
-							personDic['job_title']=job_com_entity['text'].encode('utf-8', 'ignore')
-							personDic['linkedInLink']=personDic['linkedInLink']+"&title="+personDic['job_title']
-
-						if (job_com_entity['type'] =='Company'):
-							personDic['company']=job_com_entity['text'].encode('utf-8', 'ignore')	
-							personDic['linkedInLink']=personDic['linkedInLink']+"&company="+personDic['company']						
-					# for jobentity in subresponse['entities']:
-					# 	if (jobentity['type'] =='JobTitle'):
-					# 		personDic['job_title']=jobentity['text'].encode('utf-8', 'ignore')
-
-
-
-				person_list.append(personDic)
-				#added by Xiaofeng Zhu
-
-				#removed by Xiaofeng Zhu
-				# person_list.append("name: " + entity['text'])
-				# if entity.get('disambiguated'):
-				# 	if entity['disambiguated'].get('subType'):
-				# 		person_list.append(" subType: " + entity['disambiguated']['subType'][0])
-				#removed by Xiaofeng Zhu
-
-				#if entity.get('quotations'):
-				#	print "Quotes: "
-				#	for quote in entity['quotations']:
-				#		print quote['quotation']
-			#print ""
+					name=entity['text'].encode('utf-8', 'ignore')				
+					name_quotation[name]=entity['quotations'][0]['quotation'].encode('utf-8', 'ignore')
 	else:
-		print "Error in entity call"
+		print ('Error in entity extraction call: ', people_response['statusInfo'])
+
+	previous_namelist=[]
+	response = alchemyapi.relations('url',theUrl, {'requireEntities':1,'entities':1})
+
+	if response['status'] == 'OK':
+
+		for relation in response['relations']:
+
+			# relation_entity_list=[]
+			relation_entity_dict={}
+
+			if 'subject' in relation:
+
+				if (relation['subject'].get('entities')):	
+					# print('## Subject entity ##')
+					for entity in relation['subject']['entities']:
+
+						# do not deal with multiple useful entities				
+						if entity['type'] == 'Person' or entity['type'] == 'Company'or entity['type'] == 'JobTitle':
+							relation_entity_dict[entity['type']]=entity['text'].encode('utf-8')
+							# print ('Entity type:', entity['type'])
+							# print ('Entity text:', entity['text'].encode('utf-8'))
+							# relation_entity_list.append(entity)
+
+			
+			if 'object' in relation:
+				# print('Object: ', relation['object']['text'].encode('utf-8'))
+				if (relation['object'].get('entities')):	
+					# print('## Object entity ##')
+					for entity in relation['object']['entities']:
+					
+						if entity['type'] == 'Person' or entity['type'] == 'Company'or entity['type'] == 'JobTitle':
+							relation_entity_dict[entity['type']]=entity['text'].encode('utf-8')
+							# print ('Entity type:', entity['type'])
+							# print ('Entity text:', entity['text'].encode('utf-8'))	
+							# relation_entity_list.append(entity)
+			# print ('## relation_entity_list ##')
+			if len(relation_entity_dict)>1:
+				# for key, value in relation_entity_dict.items():
+				# 	print (key, value)
+				
+				if (relation_entity_dict.has_key('Person') and len(relation_entity_dict)>1):
+					personDic={}
+					personDic['name'] = relation_entity_dict['Person'].encode('utf-8')	
+					if (personDic['name'] not in previous_namelist):
+
+						previous_namelist.append(personDic['name'])
+
+						person=personDic['name'].encode('utf-8').replace(" ", "%20")
+						personDic['linkedInLink']="https://www.linkedin.com/vsearch/p?type=people&keywords="+person	
+							
+													
+						if (relation_entity_dict.has_key('Company')):
+							personDic['company']=relation_entity_dict['Company']
+							personDic['linkedInLink']=personDic['linkedInLink']+"&company="+personDic['company']
+						else:
+							personDic['company']="N/A"
+						if (relation_entity_dict.has_key('JobTitle')):
+							personDic['job_title']=relation_entity_dict['JobTitle']
+							personDic['linkedInLink']=personDic['linkedInLink']+"&title="+personDic['job_title']
+						else:
+							personDic['job_title']="N/A"
+						personDic['twitterLink']="https://twitter.com/search?q="+person+"&src=corr&mode=users"			
+						personDic['FacebookLink']="https://www.facebook.com/search/more/?q="+person
+			
+						if (name_quotation.has_key(personDic['name'])):
+							personDic['quotation']=name_quotation[personDic['name']]	
+						else:
+							personDic['quotation']="N/A"
+
+						# for key, value in personDic.items():
+						# 	print (key, value)		
+						# print('')
+						# print('')
+						person_list.append(personDic)
+
+			# print('## relation_entity_list ##')
+			# for entity in relation_entity_list:
+			# 	print ('Entity type:', entity['type'])
+			# 	print ('Entity text:', entity['text'].encode('utf-8'))	
+			
+	else:
+		print('Error in relation extaction call: ', response['statusInfo'])
 
 	return person_list
 
