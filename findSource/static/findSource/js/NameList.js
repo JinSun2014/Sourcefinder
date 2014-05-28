5adc8a1b3635f81c1207e217935c45b30510f227
Jin Sun <callmesunjplz@gmail.com>
2014-05-28 20:31:04 +0000

    Change another method to post

--- findSource/static/findSource/js/NameList.js
+++ findSource/static/findSource/js/NameList.js
@@ -3,18 +3,11 @@ $(document).ready(function(){
     var urlsList = JSON.parse($.cookie("URLsInfo"));
     $.removeCookie("URLsList");
     var urlL = urlsList.urls.split(';');
-    urlL.pop()
+    urlL.pop();
 
-    /*var delay = (function(){
-      var timer = 0;
-      return function(callback, ms){
-        clearTimeout(timer);
-        timer = setTimeout(callback, ms);
-      };
-    })();*/
-   //console.log(urlL);
-    for (var i = 0; i != 12; i++){
-      //setTimeout(function(){
+
+    (function myloop(i){
+      setTimeout(function (){
         $.post('/Sourcerous/getInfo', {
           url: urlL[i]
         }, function(response){
@@ -30,11 +23,31 @@ $(document).ready(function(){
             alert("fail");
           }
         })
-      //}, 10000 * i);
-    }
+        if (--i) myloop(i);
+      }, 5000)
+    })(12);
+    /*
+    for (var i = 0; i != 12; i++){
+      setTimeout(function(){
+        $.post('/Sourcerous/getInfo', {
+          url: urlL[i]
+        }, function(response){
+          if (response.success){
+            var append_content = '<p>'
+            + response['title'] + '<br>'
+            + response['url'] + '<br>'
+            + response['author'] + '<br>';
+
+            append_content += '</p>';
+            $('#results').append(append_content);
+          } else {
+            alert("fail");
+          }
+        })
+      }, 10000 * i);
+    }*/
 
   });
 });
-
 
 
