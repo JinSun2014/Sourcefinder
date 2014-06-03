ae9321a9a1510617810712c73e063a9d322ca7ad
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-06-02 20:04:06 -0500

    add more people

--- findSource/AlchemyTest/alchemytest.py
+++ findSource/AlchemyTest/alchemytest.py
@@ -66,7 +66,7 @@ def GetPeople(theUrl):
 						if entity['type'] == 'Person' or entity['type'] == 'Company'or entity['type'] == 'JobTitle':
 							relation_entity_dict[entity['type']]=entity['text'].encode('utf-8')
 
-			if (relation_entity_dict.has_key('Person') and len(relation_entity_dict)>1):
+			if (relation_entity_dict.has_key('Person') and len(relation_entity_dict)>0):
 				personDic={}
 				name_key= relation_entity_dict['Person'].encode('utf-8')	
 
